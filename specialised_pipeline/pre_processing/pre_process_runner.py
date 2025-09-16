6a05cb4318af96a700a5c73ad46f16bf5d378d40
Nathan Schofield <nathan@Ludwigs-Laptop.local>
2025-09-16 15:15:06 +0100

    Pre process runner working

--- specialised_pipeline/pre_processing/pre_process_runner.py
+++ specialised_pipeline/pre_processing/pre_process_runner.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 import re
 from skimage import filters
 from scipy.ndimage import binary_fill_holes, label
@@ -18,6 +19,7 @@ fileID = '.tif'
 time = 0
 min_clus_size = 150
 
+
 base_str = "VID289"
 
 
@@ -33,11 +35,11 @@ summary_data = {eid: {"days": [], "total": [], "mean": [], "count": [], "vol_tot
 
 for experiment_id in experiment_ids:
     
-    filenames = pre_oper.generate_filenames(experiment_id="B4", base="VID289",
-                       start_day = 1, start_hour = 12, start_minute = 45, 
-                       end_day = 1, end_hour = 18, end_minute = 15,
-                       lowest_day = 1, lowest_hour = 0, lowest_minute = 0,
-                       highest_day = 1, highest_hour = 21, highest_minute = 45, 
+    filenames = pre_oper.generate_filenames(experiment_id=experiment_id, base="VID289",
+                       start_day = 0, start_hour = 0, start_minute = 0, 
+                       end_day = 5, end_hour = 0, end_minute = 0,
+                       lowest_day = 0, lowest_hour = 0, lowest_minute = 0,
+                       highest_day = 5, highest_hour = 0, highest_minute = 0, 
                        gap_days = 1, gap_hours = 3, gap_minutes = 15)
     for name in filenames:
         print(name)
@@ -49,13 +51,21 @@ for experiment_id in experiment_ids:
     total_volumes = []
     mean_volumes = []
 
+    cluster_areas = np.array([])
+
 
     # days = list(range(0, len(filenames)))
-    days = pre_oper.extract_days(filenames)
-    print("Days are", days)
+    full_times_info = pre_oper.extract_time_components(filenames)
+    times = full_times_info[0]
+    print("Times in days are", times)
 
-    # Process each day
+    # Process each timestep
+    ticker = 0
     for filename in filenames:
+        current_saving_dir_list = basedir, 'specialised_pipeline/', 'pre_processing_output/', base_str, '/'
+        current_saving_dir  =''.join(current_saving_dir_list)
+
+
         raw_arr_2D_1, raw_arr_2D_2, raw_arr_2D_3 = tif.tif_to_arr(basedir, data_folder, filename, str(time), fileID)
         arr3_normal = pre_oper.apply_rectangular_fov(raw_arr_2D_3.astype(int))
 
@@ -66,6 +76,29 @@ for experiment_id in experiment_ids:
 
         area_list = ndi_sum(current_array, labels=label_arr, index=np.arange(label_arr.max() + 1))
         area_new, index_keep = pre_oper.remove_fragments(area_list, num_clus, min_clus_size)
+            
+
+        update_area_arr = np.where(np.isin(label_arr, index_keep), label_arr, 0)
+        update_boolean_arr = update_area_arr != 0
+        update_label_arr, update_num_clus = label(update_boolean_arr)
+
+
+        # Save area array to csv file
+        df_area = pd.DataFrame(update_label_arr)
+        area_csv_name_list = current_saving_dir, filename, '_area', '.csv'
+        area_csv_name_list_2  =''.join(area_csv_name_list)
+        df_area.to_csv(area_csv_name_list_2, index=False, header=False)
+
+        # Save index array to csv file
+        df_index = pd.DataFrame(update_area_arr)
+        index_csv_name_list = current_saving_dir, filename, '_indexed', '.csv'
+        index_csv_name_list_2  =''.join(index_csv_name_list)
+        df_index.to_csv(index_csv_name_list_2, index=False, header=False)
+
+        # Add the cluster areas to 2D array of cluster sizes over time (for histogram plotting)
+        cluster_areas = pre_oper.save_clus_areas(ticker, area_new, cluster_areas)
+
+
 
 
         total_area = np.sum(area_new)
@@ -86,6 +119,7 @@ for experiment_id in experiment_ids:
 
         # plt.imshow(current_array)
         # plt.show()
+        ticker += 1
 
     # Print summary statistics
     print("Summary Statistics Over 5 Days:")
@@ -97,13 +131,55 @@ for experiment_id in experiment_ids:
 
 
 
-
-    summary_data[experiment_id]["days"] = days
+    summary_data[experiment_id]["days"] = times
     summary_data[experiment_id]["total"] = total_areas
     summary_data[experiment_id]["mean"] = mean_areas
     summary_data[experiment_id]["count"] = num_clusters
     summary_data[experiment_id]["vol_total"] = total_volumes
     summary_data[experiment_id]["vol_mean"] = mean_volumes
+
+
+    ### -----------------   Outputs  ------------------------- ###
+
+    # Save 2D cluster areas array to csv
+    df_cluster_areas = pd.DataFrame(cluster_areas)
+    cluster_areas_csv_name_list = current_saving_dir, base_str, experiment_id, '_cluster_areas', '.csv'
+    cluster_areas_csv_name_list_2  =''.join(cluster_areas_csv_name_list)
+    df_cluster_areas.to_csv(cluster_areas_csv_name_list_2, index=False, header=False)
+
+    # Save mean areas to csv
+    df_mean_areas = pd.DataFrame(mean_areas)
+    mean_areas_csv_name_list = current_saving_dir, base_str, experiment_id, '_mean_areas', '.csv'
+    mean_areas_csv_name_list_2  =''.join(mean_areas_csv_name_list)
+    df_mean_areas.to_csv(mean_areas_csv_name_list_2, index=False, header=False)
+
+    # Save total areas to csv
+    df_total_areas = pd.DataFrame(total_areas)
+    total_areas_csv_name_list = current_saving_dir, base_str, experiment_id, '_total_areas', '.csv'
+    total_areas_csv_name_list_2  =''.join(total_areas_csv_name_list)
+    df_total_areas.to_csv(total_areas_csv_name_list_2, index=False, header=False)
+
+    # Save number of clusters to csv
+    df_number_clusters = pd.DataFrame(num_clusters)
+    number_clusters_csv_name_list = current_saving_dir, base_str, experiment_id, '_number_clusters', '.csv'
+    number_clusters_csv_name_list_2  =''.join(number_clusters_csv_name_list)
+    df_number_clusters.to_csv(number_clusters_csv_name_list_2, index=False, header=False)
+
+
+
+# Export all summary data to CSV
+for eid in experiment_ids:
+    df = pd.DataFrame({
+        "Day": summary_data[eid]["days"],
+        "Total Area": summary_data[eid]["total"],
+        "Mean Area": summary_data[eid]["mean"],
+        "Cluster Count": summary_data[eid]["count"],
+        "Total Volume": summary_data[eid]["vol_total"],
+        "Mean Volume": summary_data[eid]["vol_mean"]
+    })
+    df_name_list = current_saving_dir, f"df_summary_{eid}.csv"
+    df_name = ''.join(df_name_list)
+    df.to_csv(df_name, index=False)
 
 
 
